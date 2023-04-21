d830043483ad9f7112a9338c2d1dbae5dabf1674
Matt Weiss <mweiss@northwindstech.com>
2023-04-20 22:17:02 -0500

    seo

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,7 @@ function Home() {
   return (
     <div className="home-content">
       <p>
-        Hello, we are Pointer Customs. We specialize in carpentry, remodeling & home repair in
-        South Central Wisconsin. Any home renovation project you can think of we can do! We are
-        Licensed and insured supporting the Baraboo, Sauk City and Madison Area.{' '}
-        <a href="tel:608-963-0803">Call for a quote today! (608) 963-0803</a>
+        Hello, we are Pointer Customs, your trusted experts in carpentry, remodeling, and home repair services in South Central Wisconsin. Whether you envision a stunning kitchen renovation or a modern bathroom makeover, we've got you covered! As a licensed and insured company, we proudly serve the Baraboo, Sauk City, and Madison areas. <a href="tel:608-963-0803">Get in touch for a personalized quote today! (608) 963-0803</a>
       </p>
     </div>
   );
